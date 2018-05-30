1fa9a5cdadfd130713166d910e27bf5ed16390ad
woo <kaistlp@gmail.com>
2018-05-30 14:40:43 +0000

    server setup

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
-var server = app.listen(3000,function(){
-	console.log("express server has started on port 3000")
-})
-app.get('/', function(req,res){
-res.send('hello world');
+var router = require('./router/main')(app);
+
+app.set('views',__dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine ('html', require('ejs').renderFile);
+
+
+var server = app.listen(3000, function(){
+    console.log("express server has started on port 3000")
 });
+
+app.use(express.static('public'));
