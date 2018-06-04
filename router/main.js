0c24d6929ab9455ea5998f41c8773af4f4cbdefe
woo <kaistlp@gmail.com>
2018-06-04 13:09:42 +0000

    product router js

--- router/main.js
+++ router/main.js
@@ -39,20 +39,7 @@ module.exports = function(app)
 		});
 		
 	});
-	app.get('/register',function(req,res){
-		res.render('product_register.html');
+	app.get('/product',function(req,res){
+		res.render('product.html');
 	});
-	app.get('/all',function(req,res){
-
-		connection.query('select * from persons', function (err,rows){
-			if(err) throw err;
-			console.log('the solution is : ', rows);
-			res.send(rows);
-
-
-		});
-		
-	});
-	
-
 }
