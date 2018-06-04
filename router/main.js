c5f80b05a1ea45464ff9578624beed9b7bb26104
woo <kaistlp@gmail.com>
2018-06-04 11:01:01 +0000

    register

--- router/main.js
+++ router/main.js
@@ -9,4 +9,20 @@ module.exports = function(app)
 	app.get('/signin',function(req,res){
 		res.render('signin.html');
 	});
+	app.get('/register',function(req,res){
+		res.render('product_register.html');
+	});
+	app.get('/all',function(req,res){
+
+		connection.query('select * from persons', function (err,rows){
+			if(err) throw err;
+			console.log('the solution is : ', rows);
+			res.send(rows);
+
+
+		});
+		
+	});
+	
+
 }
