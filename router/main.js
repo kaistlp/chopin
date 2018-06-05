9049c7bedb17acb4a460d832204523737dc6d91f
Kim Jaewon <zxcvbn216@kaist.ac.kr>
2018-06-06 01:39:58 +0900

    remove trash in main.js

--- router/main.js
+++ router/main.js
@@ -37,13 +37,12 @@ router.get('/signin',function(req,res){
 router.get('/signup',function(req,res){
 	res.render('signup.html');
 });
-	router.get('/persons', function(req, res){
+router.get('/persons', function(req, res){
 	connection.query('select * from persons', function(err, rows) {
-		if(err) throw err;
+	if(err) throw err;
 		console.log('the solution is : ', rows);
 		res.send(rows);
 	});
-	
 });
 router.get('/product',function(req,res){
 	res.render('product.html');
@@ -59,47 +58,3 @@ router.get('/mypage',function(req,res){
 });
 
 module.exports = router;
-
-
-/*
-module.exports = function(app)
-{
-	app.get('/',function(req,res){
-		var session = req.session;
-		if(session.username === undefined){
-			console.log("not yet logined");
-		}
-		res.render('index.html')
-	});
-	app.get('/about',function(req,res){
-		res.render('about.html');
-	});
-	app.get('/signin',function(req,res){
-		res.render('signin.html');
-	});
-	app.get('/signup',function(req,res){
-		res.render('signup.html');
-	});
-
-	app.get('/persons', function(req, res){
-		connection.query('select * from persons', function(err, rows) {
-			if(err) throw err;
-			console.log('the solution is : ', rows);
-			res.send(rows);
-		});
-		
-	});
-	app.get('/product',function(req,res){
-		res.render('product.html');
-	});
-	app.get('/upload',function(req,res){
-		res.render('upload.html');
-	});
-	app.get('/product_info',function(req,res){
-		res.render('product_info.html');
-	});
-	app.get('/mypage',function(req,res){
-		res.render('mypage.html');
-	});
-}
-*/
