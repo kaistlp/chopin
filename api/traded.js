a826abd092eb92e2315cad46d2adc83320c4f890
Paul Kim <paulkim3151@gmail.com>
2018-06-06 16:56:56 +0000

    delete copied code

--- api/traded.js
+++ api/traded.js
@@ -44,37 +44,4 @@ router.get('/all', (req, res) => {
     return;
 })
 
-
-router.get('/info/:pid', (req, res) => {
-  var pid = req.params.pid;
-  var sess = req.session;
-  console.log('find product info');
-  console.log('select pname, value from Products inner join Descriptions on Products.pid = Descriptions.pid where Products.pid = ' + pid + ' and Descriptions.name = "description";');
-
-  connection.query('select Products.pname, Products.uid, Descriptions.value from Products inner join Descriptions on Products.pid = Descriptions.pid where Products.pid = ' + pid + ' and Descriptions.name = "description";', function(err, result){
-    var response = {};
-    console.log(result);
-    if(result.length == 0){
-      response["success"] = "false";
-      response["error"] = "DB error, there aren't that product";
-      res.json(response);
-      return;
-    }
-    response["sessid"] = sess.userid;
-    response["sellid"] = result[0].uid;
-    response["pname"] = result[0].pname;
-    response["description"] = result[0].value;
-    response["success"] = "true";
-    response["error"] = "";
-    connection.query('select Users.uname, Demands.offer_price, Demands.reg_time from Demands inner join Users on Users.id = Demands.uid where Demands.pid = ' + pid + ' order by Demands.offer_price desc;', function(err, tables){
-      console.log(tables);
-      response["demands"] = tables;
-      res.json(response);
-      return;
-    })
-
-  })
-
-})
-
 module.exports = router;
