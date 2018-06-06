3d3a067fd2536dc560cd39778e7974054a671aa9
woo <kaistlp@gmail.com>
2018-06-06 14:25:59 +0000

    complete upload

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,10 +1,8 @@
 var domain = "http://localhost:3000" 
 
-
 setup();
 
 function setup() {
-	if(userid)
 	document.getElementById("upload").onclick = function() {submit()};
 }
 
@@ -12,7 +10,12 @@ function submit() {
 	var prod = document.getElementById("productInput").value;
 	var description = document.getElementById("descriptionInput").value;
 	var price = document.getElementById("maxPriceInput").value;
-	alert(prod + " " + description + " " + price);
+	var initprice = document.getElementById("initPriceInput").value
+	if(document.getElementById("maxPriceInput").disabled){
+		price = 0;
+	}
+
+	register(prod,description,price,initprice,price)
 }
 
 var i = 0;
@@ -28,6 +31,44 @@ function add_info() {
 	inputs[1].id = "ExtraValue"+i;
 	inputs[1].value = "";
 	original.parentNode.appendChild(clone);
+}
+
+function register(prod,description,initprice,price){
+	var api_url = domain + "/api/upload/new/"+prod+"/"+initprice+"/"+price;
+    $.ajax({
+        url: api_url,
+        cache: false,
+        async: false
+    }).done(function(data) {
+        if (data==0){
+        	alert("success")
+        }else{
+        	alert("fail")
+        }
+    })
+    var api_url = domain + "/api/upload/description/"+description+"/";
+    $.ajax({
+        url: api_url,
+        cache: false,
+        async: false
+    }).done(function(data) {
+    })
+    for (var options = 0;options<i+1;options+=1){
+    	var fieldname = document.getElementById("ExtraName"+options).value
+    	var fieldvalue = document.getElementById("ExtraValue"+options).value
+	    var api_url = domain + "/api/upload/option/"+fieldname+"/"+fieldvalue;
+	    if(fieldname.length>0){
+	    $.ajax({
+	        url: api_url,
+	        cache: false,
+	        async: false
+	    }).done(function(data) {
+	    })
+	    alert(options + " " + fieldname + " " + fieldname)
+		}
+    }
+    
+    
 }
 
 $("#maxPriceCheckbox").click(function(){   
