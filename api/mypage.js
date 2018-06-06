fc870e729247d76a253799e0471761dfcc42101a
woo <kaistlp@gmail.com>
2018-06-06 16:55:14 +0000

    mypage complete

--- api/mypage.js
+++ api/mypage.js
@@ -11,7 +11,7 @@ var connection = mysql.createConnection({
 });
 
 var router = express.Router();
-router.get('/', (req, res) => {
+router.get('/product', (req, res) => {
 	console.log("My Page");
 	var response = {}
 	var sess = req.session;
@@ -37,4 +37,32 @@ router.get('/', (req, res) => {
 	}
     return
 })
+
+router.get('/request', (req, res) => {
+  var response = {}
+  var sess = req.session;
+  if (!(sess.username===undefined)){
+    connection.query('select t1.uname,pname,Products.pid,is_sold,offer_price From Products INNER JOIN Users As t1 On Products.uid=t1.id INNER JOIN Demands As t2 On Products.pid=t2.pid where t2.uid In (SELECT id from Users where uname="'+sess.username+'");', function(err, result){
+        if(err){
+            console.log("listing error : " + err);
+            res.json(response)
+            return
+        }
+      console.log(result);
+
+      console.log(sess.username);
+      res.json(result)
+      return
+  })
+
+  }
+  else{
+    console.log("WHAT")
+    res.json(response)
+    return
+  }
+    return
+})
+
+
 module.exports = router;
